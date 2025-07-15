7b5a91df8850d08e93d11c27f5bdad70bf5e81d8
Jammyboyloy <jammylay07@gmail.com>
2025-07-15 15:17:29 +0700

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+// Swiper 1 (Top banner — autoplay enabled)
+const swiper1 = new Swiper('.bannerSwiper', {
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+});
+
+// Swiper 2 (Bottom section — NO autoplay)
+var swiper2 = new Swiper('.mySwiper', {
+  spaceBetween: 30,
+  hashNavigation: {
+    watchState: true,
+  },
+  navigation: {
+    nextEl: ".second-next",
+    prevEl: ".second-prev",
+  },
+});
+
+
+// AOS
+AOS.init({
+  duration: 1000, // animation duration in ms
+  once: true     // only animate once on scroll
+});
